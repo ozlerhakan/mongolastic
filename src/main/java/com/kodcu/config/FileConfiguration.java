d24122b81b7d4117a0f94224c3f81a3cb4bb9cfb
Hakan Özler <ozler.hakan@gmail.com>
2015-05-24 22:17:18 +0300

    Migrate the checkConfigArguments method to the file config

--- src/main/java/com/kodcu/config/FileConfiguration.java
+++ src/main/java/com/kodcu/config/FileConfiguration.java
@@ -5,7 +5,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.yaml.snakeyaml.Yaml;
 
-import java.io.*;
+import java.io.File;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 /**
  * Created by Hakan on 5/19/2015.
@@ -13,24 +15,64 @@ import java.io.*;
 public class FileConfiguration {
 
     private final Logger logger = LoggerFactory.getLogger(FileConfiguration.class);
-    private final String file;
+    private final String[] args;
 
-    public FileConfiguration(String file) {
-        this.file = file;
+    public FileConfiguration(String[] args) {
+        this.args = args;
     }
 
     public YamlConfiguration getFileContent() {
+        String fileName = args[0];
         YamlConfiguration config = null;
         try {
-            File configFile = new File(file);
+            File configFile = new File(fileName);
             String content = FileUtils.readFileToString(configFile, "utf-8");
             Yaml yaml = new Yaml();
             config = yaml.loadAs(content, YamlConfiguration.class);
+            if (args.length > 1)
+                this.checkConfigArguments(config);
         } catch (Exception e) {
             logger.error(e.getMessage(), e.fillInStackTrace());
         }
 
         return config;
+    }
+
+    private void checkConfigArguments(YamlConfiguration config) {
+        Class<?> clazz = config.getClass();
+        final Method[] methodList = clazz.getDeclaredMethods();
+
+        for (int i = 1; i < args.length; i++) {
+            final String[] property = args[i].split(":");
+            String key = String.join("", "set", Character.toUpperCase(property[0].charAt(0)) + property[0].substring(1));
+            String value = property[1];
+            boolean coupling = Arrays.asList(methodList).stream().anyMatch(method -> {
+                String methodName = method.getName();
+                if (methodName.equals(key)) {
+                    try {
+                        Object param = value;
+                        Class[] types = method.getParameterTypes();
+
+                        if (types[0].getName().equals("boolean"))
+                            param = Boolean.valueOf(value);
+                        else if (types[0].getName().equals("int"))
+                            param = Integer.valueOf(value);
+
+                        method.setAccessible(true);
+                        method.invoke(config, param);
+                    } catch (Exception ex) {
+                        logger.error(ex.getMessage(), ex.fillInStackTrace());
+                        return false;
+                    }
+                    return true;
+                } else
+                    return false;
+            });
+            if (!coupling) {
+                logger.error("Incorrect parameter. Pass the correct parameter name.");
+                System.exit(-1);
+            }
+        }
     }
 
 }
--- src/main/java/com/kodcu/main/Application.java
+++ src/main/java/com/kodcu/main/Application.java
@@ -8,8 +8,8 @@ import com.kodcu.converter.JSONConverter;
 import com.kodcu.service.BulkService;
 import com.kodcu.util.Constants;
 import org.apache.log4j.*;
+
 import java.io.IOException;
-import java.lang.reflect.Method;
 import java.util.Optional;
 
 /**
@@ -18,43 +18,40 @@ import java.util.Optional;
 public class Application {
 
     private static final Logger logger = Logger.getLogger(Application.class);
-    private final String fileName;
+    private final String[] args;
 
-    public Application(String file) {
-        this.fileName = file;
+    public Application(String[] args) {
+        this.args = args;
     }
 
     public static void main(String[] args) throws Exception {
         configLog();
         configAssertion(args);
-        String file = args[0];
 
-        Application app = new Application(file);
-        app.Start(args);
+        Application app = new Application(args);
+        app.Start();
     }
 
     public static void configLog() throws IOException {
         PatternLayout patternLayout = new PatternLayout();
-        FileAppender fileAppender = new FileAppender(patternLayout, "message.log", false);
+        FileAppender fileAppender = new FileAppender(patternLayout, "mongolastic.log", false);
         fileAppender.setThreshold(Level.DEBUG);
         BasicConfigurator.configure(fileAppender);
         BasicConfigurator.configure();
     }
 
-    public void Start(String[] args) {
-        FileConfiguration fConfig = new FileConfiguration(fileName);
+    public void Start() {
+        FileConfiguration fConfig = new FileConfiguration(args);
         Optional<YamlConfiguration> yamlConfig = Optional.ofNullable(fConfig.getFileContent());
-
-        if(args.length > 1)
-            yamlConfig = Optional.ofNullable(checkConfigArguments(yamlConfig.get(), args));
 
         yamlConfig.ifPresent(config -> {
             MongoConfiguration mongo = new MongoConfiguration(config);
             JSONConverter converter = new JSONConverter(mongo.getMongoCollection());
             StringBuilder bulkJSONContent = converter.buildBulkJsonFile();
-            converter.writeToFile(bulkJSONContent, config.getOutFile());
+            converter.writeToFile(bulkJSONContent, config.getOutFile(), () -> {
+                logger.info("Cool! Your bulk JSON file generated successfully!");
+            });
             mongo.closeConnection();
-            logger.info("Cool! Your bulk JSON file generated successfully!");
 
             if (config.isEnableBulk()) {
                 ElasticConfiguration client = new ElasticConfiguration(config);
@@ -65,60 +62,18 @@ public class Application {
     }
 
     static void configAssertion(String[] args) {
-
         if (args.length == 0) {
-            logger.error("Incorrect syntax. Pass the name of the file");
+            logger.error("Incorrect syntax. Pass the correct parameter(s)");
             System.exit(-1);
         }
         if (!args[0].equals(Constants.CONFIG_FILE)) {
             logger.error("It is not a config.yml file we are looking for, Where is it?");
             System.exit(-1);
         }
-    }
-
-    public YamlConfiguration checkConfigArguments(YamlConfiguration config, String[] args){
-
-        try {
-
-            Class<?> cls = Class.forName("com.kodcu.config.YamlConfiguration");
-            Method[] methlist = cls.getDeclaredMethods();
-
-            int coupling = 0;
-            String[] confParameter = null;
-
-            for(int i=1; i < args.length; i++){
-                confParameter = args[i].split(":");
-                for (int j = 0; j < methlist.length; j++) {
-                    Method m = methlist[j];
-
-                    if(m.getName().equals(confParameter[0])) {
-                        ++coupling;
-                        Object value;
-                        Class[] pvec = m.getParameterTypes();
-
-                        if(pvec.length > 0){
-                            if(pvec[0].toString().equals("boolean"))
-                                value = Boolean.valueOf(confParameter[1]);
-                            else
-                                value = confParameter[1];
-
-                            m.setAccessible(true);
-                            m.invoke(config, value);
-                        }
-                    }
-                }
-
-                if(coupling == 0){
-                    logger.error("Incorrect parameter. Pass the correct parameter name");
-                    System.exit(-1);
-                } else
-                    coupling = 0;
-            }
-
-        } catch (Exception ex){
-            logger.error(ex.getMessage(), ex.fillInStackTrace());
-        } finally {
-            return config;
+        if (args.length > 9) {
+            logger.error("Incorrect syntax. Pass less than 9 parameters");
+            System.exit(-1);
         }
     }
+
 }

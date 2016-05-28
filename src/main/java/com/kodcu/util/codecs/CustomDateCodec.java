c032a14a233943feb99bf397db868c83f4658899
winder <wwinder.unh@gmail.com>
2016-05-28 07:56:18 -0400

    Remove wrong header.

--- src/main/java/com/kodcu/util/codecs/CustomDateCodec.java
+++ src/main/java/com/kodcu/util/codecs/CustomDateCodec.java
@@ -1,17 +1,3 @@
-/**
- * DateCodec.java - Traackr, Inc.
- *
- * This document set is the property of Traackr, Inc., a Massachusetts
- * Corporation, and contains confidential and trade secret information. It
- * cannot be transferred from the custody or control of Traackr except as
- * authorized in writing by an officer of Traackr. Neither this item nor the
- * information it contains can be used, transferred, reproduced, published,
- * or disclosed, in whole or in part, directly or indirectly, except as
- * expressly authorized by an officer of Traackr, pursuant to written
- * agreement.
- *
- * Copyright 2012-2015 Traackr, Inc. All Rights Reserved.
- */
 package com.kodcu.util.codecs;
 
 import java.text.SimpleDateFormat;
